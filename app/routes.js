6f11c5b4dc3e11982b7f68e35cb77c02c3c6d5b2
Jan Mikolajczak <jan.mikolajczak@softwire.com>
2019-01-11 17:26:48 +0000

    Fix unselecting last legislation type

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,11 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+router.get('/', function(req, res, next) {
+  if(!req.query.legislation) {
+    req.session.data.legislation = []
+  }
+  next()
+})
+
 module.exports = router
